db369e6437061497c0df9c56cc404ebe10aeb098
Michele Federici (ps1dr3x) <michele@federici.tech>
2020-04-26 22:44:01 +0200

    refactor: get_next_target

--- src/worker.rs
+++ src/worker.rs
@@ -216,9 +216,45 @@ async fn tcp_custom(tx: mpsc::Sender<WorkerMessage>, target: Target, options: Op
     }
 }
 
+fn get_next_target(conf: &LacConf) -> Option<Target> {
+    if !conf.dataset.is_empty() {
+        // If dataset mode, pick a random dns record
+        // (excluding records which are not of type A)
+        let dataset_path = Path::new(conf.dataset.as_str());
+        let dataset_file = File::open(dataset_path).unwrap();
+        let mut easy_reader = EasyReader::new(dataset_file).unwrap();
+        loop {
+            let line_str = easy_reader.random_line().unwrap().unwrap();
+            let dataset_record: DatasetRecord = serde_json::from_str(&line_str).unwrap();
+            if dataset_record.record_type != "a" {
+                continue;
+            }
+            return Some(Target::new(dataset_record.name, dataset_record.value));
+        }        
+    } else {
+        // If subnet mode, pick the next ip in the specified subnets
+        let mut current_subnet_idx = conf.subnets.lock().unwrap().1;
+        let mut ip = conf.subnets.lock().unwrap().0[current_subnet_idx].next();
+        while ip.is_none() {
+            conf.subnets.lock().unwrap().1 += 1;
+            current_subnet_idx = conf.subnets.lock().unwrap().1;
+            if current_subnet_idx >= conf.subnets.lock().unwrap().0.len() {
+                break;
+            } else {
+                ip = conf.subnets.lock().unwrap().0[current_subnet_idx].next();
+            }
+        }
+        match ip {
+            Some(ip) => Some(Target::new(ip.to_string(), ip.to_string())),
+            None => None,
+        }
+    }
+}
+
 #[tokio::main]
 async fn run_async(tx: mpsc::Sender<WorkerMessage>, conf: LacConf) {
     let mut target_n = 0;
+
     let mut http = HttpConnector::new();
     http.set_connect_timeout(Some(Duration::from_secs(5)));
     http.set_happy_eyeballs_timeout(Some(Duration::from_secs(1)));
@@ -235,41 +271,7 @@ async fn run_async(tx: mpsc::Sender<WorkerMessage>, conf: LacConf) {
         .build(https);
 
     while conf.max_targets == 0 || target_n < conf.max_targets {
-        let target = if !conf.dataset.is_empty() {
-            // If dataset mode, pick a random dns record
-            // (excluding records which are not of type A)
-            let dataset_path = Path::new(conf.dataset.as_str());
-            let dataset_file = File::open(dataset_path).unwrap();
-            let mut easy_reader = EasyReader::new(dataset_file).unwrap();
-            let line_str = easy_reader.random_line().unwrap().unwrap();
-            let dataset_record: DatasetRecord = serde_json::from_str(&line_str).unwrap();
-            if dataset_record.record_type != "a" {
-                continue;
-            }
-            Some(Target::new(dataset_record.name, dataset_record.value))
-        } else {
-            // If subnet mode, pick the next ip in the specified subnets
-            let mut current_subnet_idx = conf.subnets.lock().unwrap().1;
-            let mut ip = conf.subnets.lock().unwrap().0[current_subnet_idx].next();
-            while ip.is_none() {
-                conf.subnets.lock().unwrap().1 += 1;
-                current_subnet_idx = conf.subnets.lock().unwrap().1;
-                if current_subnet_idx >= conf.subnets.lock().unwrap().0.len() {
-                    break;
-                } else {
-                    ip = conf.subnets.lock().unwrap().0[current_subnet_idx].next();
-                }
-            }
-            match ip {
-                Some(ip) => {
-                    let ip_s = ip.to_string();
-                    Some(Target::new(ip_s.clone(), ip_s))
-                }
-                None => None,
-            }
-        };
-
-        let target = if let Some(target) = target {
+        let target = if let Some(target) = get_next_target(&conf) {
             target
         } else {
             // All the targets have been consumed
